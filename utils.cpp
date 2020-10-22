1da92781b8a6a70b4e098f9122a2ce5a3e39228c
krexspace <krexspace@gmail.com>
2020-10-22 13:05:27 -0700

    initial upload

---
+++ utils.cpp
@@ -0,0 +1,36 @@
+
+#include "xyc_utils.h"
+
+namespace xyc {
+
+	using namespace std;
+	//
+	// STRING UTILS
+	//
+
+	// Splits line into a vector or ints
+	vector<int> split_to_ints(string str, char delimiter) {
+		vector<int> result;
+		stringstream ss(str); // Turn the string into a stream.
+		string tok;
+
+		while (getline(ss, tok, delimiter)) {
+			result.push_back(stoi(tok));
+		}
+
+		return result;
+	}
+
+	// Splits line into a vector or floats
+	vector<float> split_to_floats(string str, char delimiter) {
+		vector<float> result;
+		stringstream ss(str); // Turn the string into a stream.
+		string tok;
+
+		while (getline(ss, tok, delimiter)) {
+			result.push_back(stof(tok));
+		}
+
+		return result;
+	}
+}
---
+++ xyc_utils.h
@@ -0,0 +1,23 @@
+#ifndef XYC_UTILS_H
+#define XYC_UTILS_H
+
+#include <math.h>
+#include <map>
+#include <vector>
+#include <string>
+
+#include <sstream>
+
+namespace xyc {
+		//
+		// STRING UTILS
+		//
+
+		// Splits line into a vector or ints
+		std::vector<int> split_to_ints(std::string str, char delimiter);
+
+		// Splits line into a vector or floats
+		std::vector<float> split_to_floats(std::string str, char delimiter);
+}
+
+#endif
